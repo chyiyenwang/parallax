8c4dbc46df6c3455b573ad5bd35056d0c1724886
Chyi Wang <chyiyenwang@gmail.com>
2015-11-16 10:53:20 -0800

    seconde scene finished

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
   // parallax.js
   $('#scene').parallax({
+    limitX: 60,
+    limitY: 10,
+  });
+
+  $('#scene1').parallax({
+    limitX: 60,
+    limitY: 10,
   });
 
   // Fullpage.js
   $('#fullpage').fullpage({
     scrollBar: true,
     css3: true
+  });
+
+  $('#hobbes-pool').qtip({
+    content: "hello",
+    show: 'mouseover',
+    hide: 'mouseout'
   });
 })
